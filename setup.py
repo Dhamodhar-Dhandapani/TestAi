9febc52f779f0ff7ee87fff9358c6fe6f390c5c3
Dhamodhar-D <dhamodhar012005@gmail.com>
2025-03-08 19:17:25 +0530

    initial commit of project frame

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DermAi',
+    version='0.0.0',
+    author='Dhamodhar D',
+    author_email='dhamodhar1067098@gmail.com',
+    packages = find_packages(),
+    install_requires=[]
+    
+)
